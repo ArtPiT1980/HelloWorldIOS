753c2a88a525e27a37d0a873582c1c90404c622f
pt <artpit.phone@gmail.com>
2024-10-26 13:22:37 +0300

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Paul Tereshchenko on 26.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
